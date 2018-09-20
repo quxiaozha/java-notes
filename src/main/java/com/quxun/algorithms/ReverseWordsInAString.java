3337a58feb2f9c710e62405d6862ad7654df6f1f
quxiaozha <quxun1992@126.com>
2018-09-20 17:00:26 +0800

    add a String note

---
+++ src/main/java/com/quxun/algorithms/ReverseWordsInAString.java
@@ -0,0 +1,28 @@
+package com.quxun.algorithms;
+
+/**
+ * Description:
+ * Created by quxiaozha on 2018-9-20 16:39.
+ */
+public class ReverseWordsInAString {
+    public String reverseWords(String s) {
+        s = s.trim();
+        if(s.length()==0 || (!s.contains(" "))){
+            return s;
+        }
+        String[] strings = s.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for (int i = strings.length - 1; i >= 0; i--) {
+            if(!strings[i].equals("")){
+                sb.append(strings[i]+" ");
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+        System.out.println("result: ["+reverseWordsInAString.reverseWords("ssss")+"]");
+    }
+}
