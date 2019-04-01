af825d7c4ff41ecfb6a9c867f20aaaaf168c7a9b
quxiaozha <quxun1992@126.com>
2019-04-01 14:27:00 +0800

    697. Degree of an Array add another solution

--- src/main/java/com/quxun/algorithms/DegreeofanArray.java
+++ src/main/java/com/quxun/algorithms/DegreeofanArray.java
@@ -24,7 +24,7 @@ public class DegreeofanArray {
 
         int min = nums.length;
         for (int i : map.keySet()) {
-            if(max == map.get(i)[2]){
+            if (max == map.get(i)[2]) {
                 min = Math.min(min, map.get(i)[1] - map.get(i)[0] + 1);
             }
         }
@@ -32,7 +32,39 @@ public class DegreeofanArray {
         return min;
     }
 
+    public static int findShortestSubArray(int[] nums, String info) {
+        int max = nums[0];
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int len = nums.length;
+        int[] numCount = new int[max + 1];
+        int[] numFirstMeet = new int[max + 1];
+        int[] numLen = new int[max + 1];
+        int maxCount = 1;
+        for (int i = 0; i < len; i++) {
+            numCount[nums[i]]++;
+            if (numCount[nums[i]] != 1) {
+                numLen[nums[i]] = i - numFirstMeet[nums[i]] + 1;
+                maxCount = Math.max(maxCount, numCount[nums[i]]);
+            } else {
+                numFirstMeet[nums[i]] = i;
+            }
+        }
+        if (maxCount == 1) {
+            return 1;
+        }
+        int min = len;
+        for (int i = 0; i < numCount.length; i++) {
+            if (maxCount == numCount[i]) {
+                min = Math.min(min, numLen[i]);
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
-        System.out.println(findShortestSubArray(new int[] {1,2,2,3,1,4,2}));
+        System.out.println(findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2}));
+        System.out.println(findShortestSubArray(new int[]{1}, "m2"));
     }
 }
