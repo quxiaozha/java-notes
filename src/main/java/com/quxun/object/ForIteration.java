3a5f956c91d368d9554a604350d5be419a1d3fdd
quxiaozha <quxun1992@126.com>
2021-03-11 01:21:02 -0500

    add a i++&++i test for for-iteration

---
+++ src/main/java/com/quxun/object/ForIteration.java
@@ -0,0 +1,13 @@
+package com.quxun.object;
+
+public class ForIteration {
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println("---");
+        for(int i=0; i<=10; ++i){
+            System.out.println(i);
+        }
+    }
+}
