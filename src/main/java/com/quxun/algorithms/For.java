263614794537dc41e68936923a519ad17c522489
quxiaozha <quxun1992@126.com>
2023-04-03 22:01:10 +0800

    add list search and delete

--- src/main/java/com/quxun/algorithms/For.java
+++ src/main/java/com/quxun/algorithms/For.java
@@ -1,5 +1,9 @@
 package com.quxun.algorithms;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Description:
  * Created by quxiaozha on 2018-10-8 10:34.
@@ -10,5 +14,20 @@ public class For {
             i *= 2;
             System.out.println(i);//0,2,6,14
         }
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+        stringList.add("c");
+
+        for(Iterator<String> iterator = stringList.iterator(); iterator.hasNext();){
+            String value = iterator.next();
+            if("c".equals(value)){
+                iterator.remove();
+            }
+        }
+        System.out.println(stringList);
     }
 }
